e6a2b350a86365fc101f0ceecba692f4cafe7141
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-22 23:21:36 -0300

    getting address by id

--- api-laravel/app/Http/Controllers/AddressController.php
+++ api-laravel/app/Http/Controllers/AddressController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\City;
+use App\Models\State;
 use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
+
+    public function all()
+    {
+        $array = [];
+        $addresses = Address::all();
+
+        $array['status'] = 'success';
+
+        foreach($addresses as $address) {
+
+            $city = City::find($address->city);
+            $state = State::find($city->state);
+            $array['data'][] = [
+                'id' => $address->id,
+                'public_place' => $address->public_place,
+                'place_number' => $address->place_number,
+                'zip_code' => $address->zip_code,
+                'complement' => $address->complement,
+                'district' => $address->district,
+                'city' => $city->name,
+                'state' => $state->name
+            ];
+
+        }
+
+        return response()->json($array, 200);
+
+    }
+
+    public function get($id)
+    {
+        $array = [];
+        $address = Address::find($id);
+
+        if ($address) {
+
+            $city = City::find($address->city);
+            $state = State::find($city->state);
+
+            $array['status'] = 'success';
+            $array['data'] = [
+                'id' => $address->id,
+                'public_place' => $address->public_place,
+                'place_number' => $address->place_number,
+                'zip_code' => $address->zip_code,
+                'complement' => $address->complement,
+                'district' => $address->district,
+                'city' => $city->name,
+                'state' => $state->name
+            ];
+
+            return response()->json($array, 200);
+
+        } else {
+            $array['status'] = 'error';
+            $array['data'] = 'Address Not Found';
+            return response()->json($array, 404);
+        }
+    }
 
 }
