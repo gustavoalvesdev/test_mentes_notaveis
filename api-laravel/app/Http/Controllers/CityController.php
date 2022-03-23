a6d9f1954552cf7d56503af9f0e8c1a7d7f4f1f6
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-22 23:08:47 -0300

    getting city by id

--- api-laravel/app/Http/Controllers/CityController.php
+++ api-laravel/app/Http/Controllers/CityController.php
@@ -27,4 +27,29 @@ class CityController extends Controller
         return response()->json($array, 200);
 
     }
+
+    public function get($id)
+    {
+
+        $array = [];
+        $city = City::find($id);
+
+        if ($city) {
+
+            $state = State::find($city->state);
+
+            $array['status'] = 'success';
+            $array['data'] = [
+                'id' => $city->id,
+                'name' => $city->name,
+                'state' => $state->name
+            ];
+            return response()->json($array, 200);
+        } else {
+            $array['status'] = 'error';
+            $array['data'] = 'City Not Found';
+            return response()->json($array, 404);
+        }
+
+    }
 }
