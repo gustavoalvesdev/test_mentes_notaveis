af8e45f80d0af69a938a2d2bdc6a9623aa849f57
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-23 08:30:37 -0300

    get total users by state and by id

--- api-laravel/app/Http/Controllers/UserController.php
+++ api-laravel/app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\City;
 use App\Models\State;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -214,6 +215,46 @@ class UserController extends Controller
         $array['status'] = 'error';
         $array['data'] = 'User ' . $id . ' Not Found, thus can\'t be deleted.';
         return response()->json($array, 404);
+
+    }
+
+    public function getByCity($cityId)
+    {
+        $array = [];
+        $city = City::find($cityId);
+
+        if ($city) {
+            //select count(users.id) as total_usuarios_por_cidade from ((users inner join addresses on users.address = addresses.id) inner join cities on addresses.city = cities.id) where cities.id = 4;
+            $totalUSersByCity = DB::table('users')->join('addresses', 'users.address', '=', 'addresses.id')->join('cities', 'addresses.city', '=', 'cities.id')->where('cities.id', '=', $cityId);
+
+            $array['status'] = 'success';
+            $array['data'] = ['total_users_per_city' => $totalUSersByCity->count()];
+            return response()->json($array, 200);
+        } else {
+            $array['status'] = 'error';
+            $array['data'] = 'City ' . $cityId . ' Not Found';
+            return response()->json($array, 404);
+        }
+
+    }
+
+    public function getByState($stateId)
+    {
+        $array = [];
+        $city = City::find($stateId);
+
+        if ($city) {
+            //select count(users.id) as total_usuarios_por_estado from (((users inner join addresses on users.address = addresses.id) inner join cities on addresses.city = cities.id) inner join states on cities.state = states.id) where states.id = 4;
+            $totalUSersByState = DB::table('users')->join('addresses', 'users.address', '=', 'addresses.id')->join('cities', 'addresses.city', '=', 'cities.id')->join('states', 'cities.state', '=', 'states.id')->where('cities.id', '=', $stateId);
+
+            $array['status'] = 'success';
+            $array['data'] = ['total_users_per_state' => $totalUSersByState->count()];
+            return response()->json($array, 200);
+        } else {
+            $array['status'] = 'error';
+            $array['data'] = 'State ' . $stateId . ' Not Found';
+            return response()->json($array, 404);
+        }
 
     }
 }
